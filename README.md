dd182a2b2da138d93eecd77f4913ccfac089f01b
mariano <marianotorresleyva@gmail.com>
2022-05-03 12:49:18 -0400

    I am done with pug, is interesting

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # shorts-pug
+
 These files have pieces of to remenber funcitionality of pug
+
+Things that I learn - Arguments of mixins are only text/plain - Conditionals works like programming language - Use while in cases when you needed iterate many times, instead **each** is better. - pug is more util if we connect to node
---
+++ main.js
@@ -0,0 +1,12 @@
+// const imageUrl = "https://dummyimage.com/50x20/918891/0011ff.png&text=Img"
+
+// (async () => {
+//     const response = await fetch(imageUrl);
+//     const imageBlob = await response.blob();
+//     const reader = new FileReader();
+//     reader.readAsDataURL(imageBlob);
+//     reader.onloadend = () => {
+//         const base64data = reader.result;
+//         console.log(base64data);
+//     };
+// })();
